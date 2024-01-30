dc11aaae4456d159723f72fd5e526d6c74e07672
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-01-30 13:08:27 +0700

    belajar php

--- pertemuan2/index.php
+++ pertemuan2/index.php
@@ -10,3 +10,15 @@
 echo "Nathaniel Glenn"
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BELAJAR PHP</title>
+</head>
+<body>
+    <h1>Haloo, selamat datang <?php echo "Glenn" ?> </h1>
+</body>
+</html>
