944006fdcf4352cc81cf67561fa10c213c8da0c1
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-01-30 12:47:38 +0700

    belajar php

---
+++ pertemuan2/index.php
@@ -0,0 +1,12 @@
+<?php 
+// PERTEMUAN 2 PHP DASAR
+// SYNTAX PHP
+
+// STANDAR OUTPUT
+// echo, print 
+// print_r (mencetak isi array)
+// var_dump (melihat isi variabel .. tipe data dan ukuran)
+
+echo "Nathaniel Glenn"
+
+?>
